b609a88c69331dbbf189397ce09b78636ceeb0cf
KevinCrrl <y62257136@gmail.com>
2026-03-17 15:15:12 -0500

    adding .flake8 file

--- evillimiter_ng/console/io.py
+++ evillimiter_ng/console/io.py
@@ -27,7 +27,7 @@ class IO:
         """
         Print a success status message
         """
-        IO.print(f"{IO.BOLD_LIGHTGREEN }OK{IO.END_BOLD_LIGHTGREEN}   {text}", end=end)
+        IO.print(f"{IO.BOLD_LIGHTGREEN }OK{IO.END_BOLD_LIGHTGREEN}   {text}", end=end)  # noqa: E202
 
     @staticmethod
     def error(text):
